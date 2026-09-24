941c7ab86122eb023d82e28cbe0dabcc6b1350d1
Alexandr <alexandrwoody@gmail.com>
2026-09-24 22:52:36 +0300

    fix: menu theme switch localstorage

--- coffee-house/theme_switcher.js
+++ coffee-house/theme_switcher.js
@@ -2,8 +2,54 @@ const light = document.querySelector('.light');
 const dark = document.querySelector('.dark');
 const moon = document.querySelector('.moon');
 const fullmoon = document.querySelector('.fullmoon');
-dark.addEventListener('click', () => {
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.remove('light');
+    document.body.classList.add('dark');
+    dark.querySelectorAll('path').forEach(p => {
+        p.style.fill = '#b0907a;'
 
+    });
+
+    dark.querySelectorAll('rect').forEach(r => {
+        r.style.fill = '#e1d4c9';
+
+    });
+    light.querySelectorAll('rect').forEach(p => {
+        p.style.fill = '#292826';
+
+    });
+    light.querySelectorAll('path').forEach(p => {
+        p.style.fill = '#e1d4c9';
+
+    });
+}
+else {
+    document.body.classList.add('light')
+    document.body.classList.remove('light');
+    
+    dark.querySelectorAll('path').forEach(p => {
+        p.style.fill = '#b0907a;'
+
+    });
+
+    dark.querySelectorAll('rect').forEach(r => {
+        r.style.fill = '#e1d4c9';
+
+    });
+    light.querySelectorAll('rect').forEach(p => {
+        p.style.fill = '#b0907a';
+
+    });
+    light.querySelectorAll('path').forEach(p => {
+        p.style.fill = '#e1d4c9';
+
+    });
+}
+dark.addEventListener('click', () => {
+    localStorage.setItem(
+        'theme',
+        'dark' 
+    );
     document.body.classList.remove('light');
     document.body.classList.add('dark');
     dark.querySelectorAll('path').forEach(p => {
@@ -25,7 +71,10 @@ dark.addEventListener('click', () => {
     });
 });
 light.addEventListener('click', () => {
-
+    localStorage.setItem(
+        'theme',
+        'light'
+    );
     document.body.classList.remove('dark');
     document.body.classList.add('light');
     dark.querySelectorAll('path').forEach(p => {
@@ -47,3 +96,4 @@ light.addEventListener('click', () => {
     });
 
 });
+
