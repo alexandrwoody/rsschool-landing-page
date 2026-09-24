e4165897a52a669b7cc69d1def19b1b4bc53f682
Alexandr <alexandrwoody@gmail.com>
2026-09-24 18:11:36 +0300

    fix: dark theme

--- coffee-house/theme_switcher.js
+++ coffee-house/theme_switcher.js
@@ -1,20 +1,18 @@
 const light = document.querySelector('.light');
 const dark = document.querySelector('.dark');
-
-
+const moon = document.querySelector('.moon');
+const fullmoon = document.querySelector('.fullmoon');
 dark.addEventListener('click', () => {
+    if (document.body.classList.contains('light')) {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
 
-
-    document.body.classList.contains('light');
-    document.body.classList.remove('light');
-    document.body.classList.add('dark');
-
+    }
 });
 light.addEventListener('click', () => {
-
-
-    document.body.classList.contains('dark');
-    document.body.classList.remove('dark');
-    document.body.classList.add('light');
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+    }
 
 });
