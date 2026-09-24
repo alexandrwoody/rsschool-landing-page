7367a6df00769da62fb9e2f9f37b9243c5e0e843
Alexandr <alexandrwoody@gmail.com>
2026-09-24 20:36:28 +0300

    fix: dark theme icons

--- coffee-house/theme_switcher.js
+++ coffee-house/theme_switcher.js
@@ -3,16 +3,47 @@ const dark = document.querySelector('.dark');
 const moon = document.querySelector('.moon');
 const fullmoon = document.querySelector('.fullmoon');
 dark.addEventListener('click', () => {
-    if (document.body.classList.contains('light')) {
-        document.body.classList.remove('light');
-        document.body.classList.add('dark');
 
-    }
+    document.body.classList.remove('light');
+    document.body.classList.add('dark');
+    dark.querySelectorAll('path').forEach(p => {
+        p.style.fill = '#b0907a;'
+
+    });
+
+    dark.querySelectorAll('rect').forEach(r => {
+        r.style.fill = '#e1d4c9';
+
+    });
+    light.querySelectorAll('rect').forEach(p => {
+        p.style.fill = '#292826';
+
+    });
+    light.querySelectorAll('path').forEach(p => {
+        p.style.fill = '#e1d4c9';
+
+    });
 });
 light.addEventListener('click', () => {
-    if (document.body.classList.contains('dark')) {
-        document.body.classList.remove('dark');
-        document.body.classList.add('light');
-    }
+
+    document.body.classList.remove('dark');
+    document.body.classList.add('light');
+    dark.querySelectorAll('path').forEach(p => {
+        p.style.fill = '#292826';
+
+    });
+
+    dark.querySelectorAll('rect').forEach(r => {
+        r.style.fill = '#e1d4c9';
+
+    });
+    light.querySelectorAll('rect').forEach(p => {
+        p.style.fill = '#b0907a';
+
+    });
+    light.querySelectorAll('path').forEach(p => {
+        p.style.fill = '#e1d4c9';
+
+    });
 
 });
